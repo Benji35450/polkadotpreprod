f6db0d4ea6c1a278673f295ae0a1685116baafcc
Benji35450 <brochardbenjamin.pro@gmail.com>
2022-09-08 13:07:56 +0200

    Initial commit

---
+++ extensions/Translate/tag/TranslatablePage.php
@@ -0,0 +1,597 @@
+<?php
+
+use MediaWiki\Extension\Translate\MessageGroupProcessing\RevTagStore;
+use MediaWiki\Extension\Translate\MessageGroupProcessing\TranslatableBundle;
+use MediaWiki\Extension\Translate\PageTranslation\TranslationPage;
+use MediaWiki\Extension\Translate\Services;
+use MediaWiki\Linker\LinkTarget;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Revision\RevisionLookup;
+use MediaWiki\Revision\RevisionRecord;
+use MediaWiki\Revision\SlotRecord;
+use Wikimedia\Rdbms\Database;
+
+/**
+ * Mixed bag of methods related to translatable pages.
+ *
+ * @author Niklas Laxström
+ * @license GPL-2.0-or-later
+ * @ingroup PageTranslation
+ */
+class TranslatablePage extends TranslatableBundle {
+	/**
+	 * List of keys in the metadata table that need to be handled for moves and deletions
+	 * @phpcs-require-sorted-array
+	 */
+	public const METADATA_KEYS = [
+		'maxid',
+		'priorityforce',
+		'prioritylangs',
+		'priorityreason',
+		'transclusion',
+		'version'
+	];
+	/** @var string Name of the section which contains the translated page title. */
+	public const DISPLAY_TITLE_UNIT_ID = 'Page display title';
+
+	/** @var Title */
+	protected $title;
+	/** @var RevTagStore */
+	protected $revTagStore;
+	/** @var ?string Text contents of the page. */
+	protected $text;
+	/** @var ?int Revision of the page, if applicable. */
+	protected $revision;
+	/** @var string From which source this object was constructed: text, revision or title */
+	protected $source;
+	/** @var ?bool Whether the title should be translated */
+	protected $pageDisplayTitle;
+	/** @var ?string */
+	private $targetLanguage;
+
+	/** @param Title $title Title object for the page */
+	protected function __construct( Title $title ) {
+		$this->title = $title;
+		$this->revTagStore = new RevTagStore();
+	}
+
+	/**
+	 * Constructs a translatable page from given text.
+	 * Some functions will fail unless you set revision
+	 * parameter manually.
+	 *
+	 * @param Title $title
+	 * @param string $text
+	 *
+	 * @return self
+	 */
+	public static function newFromText( Title $title, $text ) {
+		$obj = new self( $title );
+		$obj->text = $text;
+		$obj->source = 'text';
+
+		return $obj;
+	}
+
+	/**
+	 * Constructs a translatable page from given revision.
+	 * The revision must belong to the title given or unspecified
+	 * behavior will happen.
+	 *
+	 * @param Title $title
+	 * @param int $revision Revision number
+	 * @throws MWException
+	 * @return self
+	 */
+	public static function newFromRevision( Title $title, int $revision ) {
+		$rev = MediaWikiServices::getInstance()
+			->getRevisionLookup()
+			->getRevisionByTitle( $title, $revision );
+		if ( $rev === null ) {
+			throw new MWException( 'Revision is null' );
+		}
+
+		$obj = new self( $title );
+		$obj->source = 'revision';
+		$obj->revision = $revision;
+
+		return $obj;
+	}
+
+	/**
+	 * Constructs a translatable page from title.
+	 * The text of last marked revision is loaded when needed.
+	 *
+	 * @param Title $title
+	 * @return self
+	 */
+	public static function newFromTitle( Title $title ) {
+		$obj = new self( $title );
+		$obj->source = 'title';
+
+		return $obj;
+	}
+
+	/** @inheritDoc */
+	public function getTitle(): Title {
+		return $this->title;
+	}
+
+	/**
+	 * Returns the text for this translatable page.
+	 * @return string
+	 */
+	public function getText(): string {
+		if ( $this->text !== null ) {
+			return $this->text;
+		}
+
+		$page = $this->getTitle()->getPrefixedDBkey();
+
+		if ( $this->source === 'title' ) {
+			$revision = $this->getMarkedTag();
+			if ( !is_int( $revision ) ) {
+				throw new LogicException(
+					"Trying to load a text for $page which is not marked for translation"
+				);
+			}
+			$this->revision = $revision;
+		}
+
+		$flags = TranslateUtils::shouldReadFromPrimary()
+			? RevisionLookup::READ_LATEST
+			: RevisionLookup::READ_NORMAL;
+		$rev = MediaWikiServices::getInstance()
+			->getRevisionLookup()
+			->getRevisionByTitle( $this->getTitle(), $this->revision, $flags );
+		$content = $rev->getContent( SlotRecord::MAIN );
+		$text = ( $content instanceof TextContent ) ? $content->getText() : null;
+
+		if ( !is_string( $text ) ) {
+			throw new RuntimeException( "Failed to load text for $page" );
+		}
+
+		$this->text = $text;
+
+		return $this->text;
+	}
+
+	/**
+	 * Revision is null if object was constructed using newFromText.
+	 * @return null|int
+	 */
+	public function getRevision(): ?int {
+		return $this->revision;
+	}
+
+	/**
+	 * Returns the source language of this translatable page. In other words
+	 * the language in which the page without language code is written.
+	 * @return string
+	 * @since 2013-01-28
+	 */
+	public function getSourceLanguageCode() {
+		return $this->getTitle()->getPageLanguage()->getCode();
+	}
+
+	/** @inheritDoc */
+	public function getMessageGroupId(): string {
+		return self::getMessageGroupIdFromTitle( $this->getTitle() );
+	}
+
+	/**
+	 * Constructs MessageGroup id for any title.
+	 * @param Title $title
+	 * @return string
+	 */
+	public static function getMessageGroupIdFromTitle( Title $title ) {
+		return 'page-' . $title->getPrefixedText();
+	}
+
+	/**
+	 * Returns MessageGroup used for translating this page. It may still be empty
+	 * if the page has not been ever marked.
+	 * @return WikiPageMessageGroup
+	 */
+	public function getMessageGroup() {
+		return MessageGroups::getGroup( $this->getMessageGroupId() );
+	}
+
+	/**
+	 * Check whether title is marked for translation
+	 * @return bool
+	 * @since 2014.06
+	 */
+	public function hasPageDisplayTitle() {
+		// Cached value
+		if ( $this->pageDisplayTitle !== null ) {
+			return $this->pageDisplayTitle;
+		}
+
+		// Check if title section exists in list of sections
+		$factory = Services::getInstance()->getTranslationUnitStoreFactory();
+		$store = $factory->getReader( $this->getTitle() );
+		$this->pageDisplayTitle = in_array( self::DISPLAY_TITLE_UNIT_ID, $store->getNames() );
+
+		return $this->pageDisplayTitle;
+	}
+
+	/**
+	 * Get translated page title.
+	 * @param string $code Language code.
+	 * @return string|null
+	 */
+	public function getPageDisplayTitle( $code ) {
+		// Return null if title not marked for translation
+		if ( !$this->hasPageDisplayTitle() ) {
+			return null;
+		}
+
+		// Display title from DB
+		$section = str_replace( ' ', '_', self::DISPLAY_TITLE_UNIT_ID );
+		$page = $this->getTitle()->getPrefixedDBkey();
+
+		$group = $this->getMessageGroup();
+		// Sanity check, seems to happen during moves
+		if ( !$group instanceof WikiPageMessageGroup ) {
+			return null;
+		}
+
+		return $group->getMessage( "$page/$section", $code, $group::READ_NORMAL );
+	}
+
+	/**
+	 * @return string
+	 * @since 2020.07
+	 */
+	public function getStrippedSourcePageText(): string {
+		$parser = Services::getInstance()->getTranslatablePageParser();
+		$text = $parser->cleanupTags( $this->getText() );
+		$text = preg_replace( '~<languages\s*/>\n?~s', '', $text );
+
+		return $text;
+	}
+
+	/**
+	 * @param Title $title
+	 * @return ?TranslationPage
+	 * @since 2020.07
+	 */
+	public static function getTranslationPageFromTitle( Title $title ): ?TranslationPage {
+		$self = self::isTranslationPage( $title );
+		if ( !$self ) {
+			return null;
+		}
+
+		return $self->getTranslationPage( $self->targetLanguage );
+	}
+
+	/**
+	 * @param string $targetLanguage
+	 * @return TranslationPage
+	 * @since 2020.07
+	 */
+	public function getTranslationPage( string $targetLanguage ): TranslationPage {
+		$config = MediaWikiServices::getInstance()->getMainConfig();
+		$parser = Services::getInstance()->getTranslatablePageParser();
+		$parserOutput = $parser->parse( $this->getText() );
+		$pageVersion = (int)TranslateMetadata::get( $this->getMessageGroupId(), 'version' );
+		$wrapUntranslated = $pageVersion >= 2;
+
+		return new TranslationPage(
+			$parserOutput,
+			$this->getMessageGroup(),
+			Language::factory( $targetLanguage ),
+			Language::factory( $this->getSourceLanguageCode() ),
+			$config->get( 'TranslateKeepOutdatedTranslations' ),
+			$wrapUntranslated,
+			$this->getTitle()
+		);
+	}
+
+	protected static $tagCache = [];
+
+	/**
+	 * Adds a tag which indicates that this page is
+	 * suitable for translation.
+	 * @param int $revision
+	 * @param null|string $value
+	 */
+	public function addMarkedTag( int $revision, $value = null ) {
+		$this->revTagStore->replaceTag( $this->getTitle(), 'tp:mark', $revision, $value );
+		self::clearSourcePageCache();
+	}
+
+	/**
+	 * Adds a tag which indicates that this page source is
+	 * ready for marking for translation.
+	 * @param int $revision
+	 */
+	public function addReadyTag( int $revision ): void {
+		$this->revTagStore->replaceTag( $this->getTitle(), 'tp:tag', $revision );
+		if ( !self::isSourcePage( $this->getTitle() ) ) {
+			self::clearSourcePageCache();
+		}
+	}
+
+	/**
+	 * Returns the latest revision which has marked tag, if any.
+	 * @return ?int
+	 */
+	public function getMarkedTag() {
+		return $this->revTagStore->getLatestRevisionWithTag( $this->getTitle(), 'tp:mark' );
+	}
+
+	/**
+	 * Returns the latest revision which has ready tag, if any.
+	 * @return ?int
+	 */
+	public function getReadyTag(): ?int {
+		return $this->revTagStore->getLatestRevisionWithTag( $this->getTitle(), 'tp:tag' );
+	}
+
+	/**
+	 * Removes all page translation feature data from the database.
+	 * Does not remove translated sections or translation pages.
+	 */
+	public function unmarkTranslatablePage() {
+		$aid = $this->getTitle()->getArticleID();
+		$dbw = wfGetDB( DB_PRIMARY );
+
+		$this->revTagStore->removeTags( $this->getTitle(), 'tp:mark', 'tp:tag' );
+		$dbw->delete( 'translate_sections', [ 'trs_page' => $aid ], __METHOD__ );
+
+		self::clearSourcePageCache();
+	}
+
+	/**
+	 * Produces a link to translation view of a translation page.
+	 * @param string|bool $code MediaWiki language code. Default: false.
+	 * @return string Relative url
+	 */
+	public function getTranslationUrl( $code = false ) {
+		$params = [
+			'group' => $this->getMessageGroupId(),
+			'action' => 'page',
+			'filter' => '',
+			'language' => $code,
+		];
+
+		$translate = SpecialPage::getTitleFor( 'Translate' );
+
+		return $translate->getLocalURL( $params );
+	}
+
+	public function getMarkedRevs() {
+		$db = TranslateUtils::getSafeReadDB();
+
+		$fields = [ 'rt_revision', 'rt_value' ];
+		$conds = [
+			'rt_page' => $this->getTitle()->getArticleID(),
+			'rt_type' => RevTag::getType( 'tp:mark' ),
+		];
+		$options = [ 'ORDER BY' => 'rt_revision DESC' ];
+
+		return $db->select( 'revtag', $fields, $conds, __METHOD__, $options );
+	}
+
+	/** @inheritDoc */
+	public function getTranslationPages(): array {
+		$mwServices = MediaWikiServices::getInstance();
+		$knownLanguageCodes = $this->getMessageGroup()->getTranslatableLanguages()
+			?? TranslateUtils::getLanguageNames( null );
+
+		$prefixedDbTitleKey = $this->getTitle()->getDBkey() . '/';
+		$baseNamespace = $this->getTitle()->getNamespace();
+
+		// Build a link batch query for all translation pages
+		$linkBatch = $mwServices->getLinkBatchFactory()->newLinkBatch();
+		foreach ( array_keys( $knownLanguageCodes ) as $code ) {
+			$linkBatch->add( $baseNamespace, $prefixedDbTitleKey . $code );
+		}
+
+		$translationPages = [];
+		foreach ( $linkBatch->getPageIdentities() as $pageIdentity ) {
+			if ( $pageIdentity->exists() ) {
+				$translationPages[] = Title::castFromPageIdentity( $pageIdentity );
+			}
+		}
+
+		return $translationPages;
+	}
+
+	/** @inheritDoc */
+	public function getTranslationUnitPages( ?string $code = null ): array {
+		return $this->getTranslationUnitPagesByTitle( $this->title, $code );
+	}
+
+	/** @return array */
+	public function getTranslationPercentages() {
+		// Calculate percentages for the available translations
+		$group = $this->getMessageGroup();
+		if ( !$group instanceof WikiPageMessageGroup ) {
+			return [];
+		}
+
+		$titles = $this->getTranslationPages();
+		$temp = MessageGroupStats::forGroup( $this->getMessageGroupId() );
+		$stats = [];
+
+		foreach ( $titles as $t ) {
+			$handle = new MessageHandle( $t );
+			$code = $handle->getCode();
+
+			// Sometimes we want to display 0.00 for pages for which translation
+			// hasn't started yet.
+			$stats[$code] = 0.00;
+			if ( ( $temp[$code][MessageGroupStats::TOTAL] ?? 0 ) > 0 ) {
+				$total = $temp[$code][MessageGroupStats::TOTAL];
+				$translated = $temp[$code][MessageGroupStats::TRANSLATED];
+				$percentage = $translated / $total;
+				$stats[$code] = sprintf( '%.2f', $percentage );
+			}
+		}
+
+		// Content language is always up-to-date
+		$stats[$this->getSourceLanguageCode()] = 1.00;
+
+		return $stats;
+	}
+
+	public function getTransRev( $suffix ) {
+		$title = Title::makeTitle( NS_TRANSLATIONS, $suffix );
+
+		$db = TranslateUtils::getSafeReadDB();
+		$fields = 'rt_value';
+		$conds = [
+			'rt_page' => $title->getArticleID(),
+			'rt_type' => RevTag::getType( 'tp:transver' ),
+		];
+		$options = [ 'ORDER BY' => 'rt_revision DESC' ];
+
+		return $db->selectField( 'revtag', $fields, $conds, __METHOD__, $options );
+	}
+
+	public function supportsTransclusion(): ?bool {
+		$transclusion = TranslateMetadata::get( $this->getMessageGroupId(), 'transclusion' );
+		if ( $transclusion === false ) {
+			return null;
+		}
+
+		return $transclusion === '1';
+	}
+
+	public function setTransclusion( bool $supportsTransclusion ): void {
+		TranslateMetadata::set(
+			$this->getMessageGroupId(),
+			'transclusion',
+			$supportsTransclusion ? '1' : '0'
+		);
+	}
+
+	public function getRevisionRecordWithFallback(): ?RevisionRecord {
+		$title = $this->getTitle();
+		$store = MediaWikiServices::getInstance()->getRevisionStore();
+		$revRecord = $store->getRevisionByTitle( $title->getSubpage( $this->targetLanguage ) );
+		if ( $revRecord ) {
+			return $revRecord;
+		}
+
+		// Fetch the source fallback
+		$sourceLanguage = $this->getMessageGroup()->getSourceLanguage();
+		return $store->getRevisionByTitle( $title->getSubpage( $sourceLanguage ) );
+	}
+
+	/** @inheritDoc */
+	public function isMoveable(): bool {
+		return $this->getMarkedTag() !== null;
+	}
+
+	/** @inheritDoc */
+	public function isDeletable(): bool {
+		return $this->getMarkedTag() !== null;
+	}
+
+	/**
+	 * @param Title $title
+	 * @return bool|self
+	 */
+	public static function isTranslationPage( Title $title ) {
+		$handle = new MessageHandle( $title );
+		$key = $handle->getKey();
+		$code = $handle->getCode();
+
+		if ( $key === '' || $code === '' ) {
+			return false;
+		}
+
+		$codes = MediaWikiServices::getInstance()->getLanguageNameUtils()->getLanguageNames();
+		global $wgTranslateDocumentationLanguageCode;
+		unset( $codes[$wgTranslateDocumentationLanguageCode] );
+
+		if ( !isset( $codes[$code] ) ) {
+			return false;
+		}
+
+		$newtitle = self::changeTitleText( $title, $key );
+
+		if ( !$newtitle ) {
+			return false;
+		}
+
+		$page = self::newFromTitle( $newtitle );
+
+		if ( $page->getMarkedTag() === null ) {
+			return false;
+		}
+
+		$page->targetLanguage = $code;
+
+		return $page;
+	}
+
+	protected static function changeTitleText( Title $title, $text ) {
+		return Title::makeTitleSafe( $title->getNamespace(), $text );
+	}
+
+	/**
+	 * Helper to guess translation page from translation unit.
+	 *
+	 * @param LinkTarget $translationUnit
+	 * @return array
+	 * @since 2019.10
+	 */
+	public static function parseTranslationUnit( LinkTarget $translationUnit ): array {
+		// Format is Translations:SourcePageNamespace:SourcePageName/SectionName/LanguageCode.
+		// We will drop the namespace immediately here.
+		$parts = explode( '/', $translationUnit->getText() );
+
+		// LanguageCode and SectionName are guaranteed to not have '/'.
+		$language = array_pop( $parts );
+		$section = array_pop( $parts );
+		$sourcepage = implode( '/', $parts );
+
+		return [
+			'sourcepage' => $sourcepage,
+			'section' => $section,
+			'language' => $language
+		];
+	}
+
+	/**
+	 * @param Title $title
+	 * @return bool
+	 */
+	public static function isSourcePage( Title $title ) {
+		$cache = MediaWikiServices::getInstance()->getMainWANObjectCache();
+		$cacheKey = $cache->makeKey( 'pagetranslation', 'sourcepages' );
+
+		$translatablePageIds = $cache->getWithSetCallback(
+			$cacheKey,
+			$cache::TTL_HOUR * 2,
+			static function ( $oldValue, &$ttl, array &$setOpts ) {
+				$dbr = wfGetDB( DB_REPLICA );
+				$setOpts += Database::getCacheSetOptions( $dbr );
+
+				return RevTagStore::getTranslatableBundleIds(
+					RevTag::getType( 'tp:mark' ), RevTag::getType( 'tp:tag' )
+				);
+			},
+			[
+				'checkKeys' => [ $cacheKey ],
+				'pcTTL' => $cache::TTL_PROC_SHORT,
+				'pcGroup' => __CLASS__ . ':1'
+			]
+		);
+
+		return in_array( $title->getArticleID(), $translatablePageIds );
+	}
+
+	/**
+	 * Clears the source page cache
+	 */
+	public static function clearSourcePageCache(): void {
+		$cache = MediaWikiServices::getInstance()->getMainWANObjectCache();
+		$cache->touchCheckKey( $cache->makeKey( 'pagetranslation', 'sourcepages' ) );
+	}
+}
---
+++ extensions/Translate/tag/TranslateRenderJob.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * Job for updating translation pages.
+ *
+ * @file
+ * @author Niklas Laxström
+ * @license GPL-2.0-or-later
+ */
+
+use MediaWiki\Extension\Translate\Jobs\GenericTranslateJob;
+use MediaWiki\Extension\Translate\SystemUsers\FuzzyBot;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Permissions\Authority;
+use MediaWiki\User\UserIdentity;
+
+/**
+ * Job for updating translation pages when translation or template changes.
+ *
+ * @ingroup PageTranslation JobQueue
+ */
+class TranslateRenderJob extends GenericTranslateJob {
+	public const ACTION_DELETE = 'delete';
+
+	public static function newJob( Title $target, ?string $triggerAction = null ): self {
+		$job = new self( $target, [ 'triggerAction' => $triggerAction ] );
+		$job->setUser( FuzzyBot::getUser() );
+		$job->setFlags( EDIT_FORCE_BOT );
+		$job->setSummary( wfMessage( 'tpt-render-summary' )->inContentLanguage()->text() );
+
+		return $job;
+	}
+
+	/**
+	 * @param Title $title
+	 * @param array $params
+	 */
+	public function __construct( $title, $params = [] ) {
+		parent::__construct( __CLASS__, $title, $params );
+		$this->removeDuplicates = true;
+	}
+
+	public function run(): bool {
+		$this->logInfo( 'Starting TranslateRenderJob' );
+
+		// We may be doing double wait here if this job was spawned by TranslationUpdateJob
+		$lb = MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
+		if ( !$lb->waitForReplication() ) {
+			$this->logWarning( 'Continuing despite replication lag' );
+		}
+
+		// Initialization
+		$title = $this->title;
+
+		$tpPage = TranslatablePage::getTranslationPageFromTitle( $title );
+		if ( !$tpPage ) {
+			$this->logError( 'Cannot render translation page!' );
+			return false;
+		}
+
+		// Other stuff
+		$user = $this->getUser();
+		$summary = $this->getSummary();
+		$flags = $this->getFlags();
+
+		// We should not re-create the translation page if a translation unit is being deleted
+		// because it is possible that the translation page may also be queued for deletion.
+		// Hence set the flag to EDIT_UPDATE and remove EDIT_NEW if its added
+		if ( $this->isDeleteTrigger() ) {
+			$flags = ( $flags | EDIT_UPDATE ) & ~EDIT_NEW;
+		}
+
+		$page = MediaWikiServices::getInstance()->getWikiPageFactory()->newFromTitle( $title );
+
+		// @todo FuzzyBot hack
+		PageTranslationHooks::$allowTargetEdit = true;
+		$content = $tpPage->getPageContent();
+		$editStatus = $page->doUserEditContent(
+			$content,
+			$user,
+			$summary,
+			$flags
+		);
+		if ( !$editStatus->isOK() ) {
+			if ( $this->isDeleteTrigger() && $editStatus->hasMessage( 'edit-gone-missing' ) ) {
+				$this->logInfo( 'Translation page missing with delete trigger' );
+			} else {
+				$this->logError(
+					'Error while editing content in page.',
+					[
+						'content' => $content->getTextForSummary(),
+						'errors' => $editStatus->getErrors()
+					]
+				);
+			}
+		}
+
+		$this->logInfo( 'Finished page edit operation' );
+		PageTranslationHooks::$allowTargetEdit = false;
+
+		$this->logInfo( 'Finished TranslateRenderJob' );
+		return true;
+	}
+
+	/** @param int $flags */
+	public function setFlags( $flags ) {
+		$this->params['flags'] = $flags;
+	}
+
+	/** @return int */
+	private function getFlags() {
+		return $this->params['flags'];
+	}
+
+	/** @param string $summary */
+	public function setSummary( $summary ) {
+		$this->params['summary'] = $summary;
+	}
+
+	/** @return string */
+	private function getSummary() {
+		return $this->params['summary'];
+	}
+
+	/** @param UserIdentity|string $user */
+	public function setUser( $user ) {
+		if ( $user instanceof UserIdentity ) {
+			$this->params['user'] = $user->getName();
+		} else {
+			$this->params['user'] = $user;
+		}
+	}
+
+	/**
+	 * Get a user object for doing edits.
+	 * @return Authority
+	 */
+	private function getUser() {
+		return User::newFromName( $this->params['user'], false );
+	}
+
+	private function isDeleteTrigger(): bool {
+		$triggerAction = $this->params['triggerAction'] ?? null;
+		return $triggerAction === self::ACTION_DELETE;
+	}
+}
